2d1897085ac23b9def323fb049ca0035c5c7b53e
BOYER AXEL <axel.boyer2@etu.unistra.fr>
2024-02-12 23:43:03 +0100

    Added token & handled security - started playlist API endpoint

--- packages/server/src/context.ts
+++ packages/server/src/context.ts
@@ -2,9 +2,6 @@ import { initTRPC } from "@trpc/server";
 import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { decodeAndVerifyJwtToken } from "./utils/auth";
 export async function createContext({ req, res }) {
-  // Create your context based on the request object
-  // Will be available as `ctx` in all your resolvers
-  // This is just an example of something you might want to do in your ctx fn
   async function getUserFromHeader() {
     if (req.headers.authorization) {
       const user = await decodeAndVerifyJwtToken(
--- packages/server/src/routers/user.ts
+++ packages/server/src/routers/user.ts
@@ -3,6 +3,8 @@ import { publicProcedure, router } from "../trpc";
 import { prisma } from "../app";
 import { TRPCError } from "@trpc/server";
 import bcrypt from "bcrypt";
+import fs from "fs";
+import jsonwebtoken from "jsonwebtoken";
 export const userRouter = router({
   register: publicProcedure
     .input(
@@ -54,5 +56,47 @@ export const userRouter = router({
         password: z.string(),
       })
     )
-    .query(async (opts) => {}),
+    .query(async (opts) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: opts.input.email,
+        },
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          password: true,
+        },
+      });
+      if (!user) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid email or password.",
+        });
+      }
+      const passwordMatch = await bcrypt.compare(
+        opts.input.password,
+        user.password
+      );
+      if (!passwordMatch) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid email or password.",
+        });
+      }
+      // Creation of JWT token
+      const secretKey = fs.readFileSync("../keys/private.key", "utf8");
+      const payload = {
+        id: user.id,
+        email: user.email,
+      };
+      const options = {
+        expiresIn: "5h",
+      };
+      const token = jsonwebtoken.sign(payload, secretKey, options);
+      return {
+        user: user,
+        token: token,
+      };
+    }),
 });
